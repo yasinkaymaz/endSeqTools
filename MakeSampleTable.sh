bd1662b5fcda5bfc2fb112052d61acadb908cbab
Yasin Kaymaz <yasinkaymaz@github.com>
2017-12-11 15:12:44 -0500

    running test

--- MakeSampleTable.sh
+++ MakeSampleTable.sh
@@ -17,12 +17,12 @@ module load bedtools/2.17.0
 module load python/2.7.5
 module load fastqc/0.10.1
 
-endSeqDir='/project/umw_jeffrey_bailey/OTHERS/endSeq_Tools'
-export PATH=/project/umw_jeffrey_bailey/OTHERS/endSeq_Tools/src:$PATH
-export PATH=/project/umw_jeffrey_bailey/OTHERS/endSeq_Tools/utils:$PATH
+endSeqDir='/home/yk42w/codes/endSeq_Tools'
+export PATH=/home/yk42w/codes/endSeq_Tools/src:$PATH
+export PATH=/home/yk42w/codes/endSeq_Tools/utils:$PATH
 #TO DO:
 #----------
-# 1) PROVIDE AN APPROPRIATE FILE "/Full-directory/file_contains_sample_fastqs.txt". This file should contain each fastq file for the samples with a unique name and full directories at each line. 
+# 1) PROVIDE AN APPROPRIATE FILE "/Full-directory/file_contains_sample_fastqs.txt". This file should contain each fastq file for the samples with a unique name and full directories at each line.
 #	For example; file_contains_sample_fastqs.txt file should look like this;
 #	/project/umw_jeffrey_bailey/OTHERS/Ami/rerun/N0.fastq
 #	/project/umw_jeffrey_bailey/OTHERS/Ami/rerun/N4.fastq
@@ -33,8 +33,14 @@ export PATH=/project/umw_jeffrey_bailey/OTHERS/endSeq_Tools/utils:$PATH
 # 2) Modify the script below; give the correct "/Full-directory/" for "file_contains_sample_fastqs.txt" file as well as *.gtf file for ex; gencode.v19.annotation.gtf.
 # 3) SUBMIT THIS SCRIPT BY DOING THIS: bsub < MakeSampleTable.sh
 
-python $endSeqDir/endSeq_tools.py makeSamplesTable -sf /Full-directory/file_contains_sample_fastqs.txt --gtf /Full-directory/gencode.v19.annotation.gtf
+#python $endSeqDir/endSeq_tools.py makeSamplesTable -sf /Full-directory/file_contains_sample_fastqs.txt --gtf /Full-directory/gencode.v19.annotation.gtf
+DIR='/project/umw_jeffrey_bailey/OTHERS/LINGTAO/new_bed_nbpred3'
 
+python $endSeqDir/endSeq_tools.py makeSamplesTable -sf $DIR/file_contains_sample_fastqs_YK.txt --gtf /project/umw_jeffrey_bailey/share/Homo_sapiens/Gencode/v19/gencode.v19.annotation.gtf
+
+python $endSeqDir/endSeq_tools.py annotatePeaks -bd mergedPeaks_CPM.bed
+
+#Rscript $endSeqDir/utils/PASseq_chisquire_beta_v4.R $DIR/mergedPeaks_annotated_CPM-3.bed $DIR/switch_Test.txt 6 8 $DIR
 
 #In summary, this function will
 #1) Collect all non-zero genomic locations from all samples into a pool,
@@ -43,9 +49,7 @@ python $endSeqDir/endSeq_tools.py makeSamplesTable -sf /Full-directory/file_cont
 #4) Exclude PA sites of which cumulative read counts do not exceed to 100 reads (this is sum of all samples at that particular site so the threshold is not that high sample wise)
 #5) Normalize each site expression by the total number of reads comprising real PA sites (after filtrations) in each sample separately (Counts Per Million),
 #6) Calculate percent PA site usage within gene for each PA site.
-#7) Output sample matrices in three different format after annotating with gene names; 
+#7) Output sample matrices in three different format after annotating with gene names;
 #        mergedPeaks_annotated_Count.bed
 #        mergedPeaks_annotated_CPM.bed
 #        mergedPeaks_annotated_Percent.bed
-
-
